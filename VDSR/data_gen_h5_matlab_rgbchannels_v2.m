10f2e8c6eedb65c14a4af3b4972da4b1967c22cb
Shreyas Gokhale <55978108+gokhaleshreyas@users.noreply.github.com>
2021-07-22 13:58:35 +0530

    Add files via upload

---
+++ VDSR/data_gen_h5_matlab_rgbchannels_v2.m
@@ -0,0 +1,115 @@
+clc;
+clear all;
+close all;
+
+image_size = 41;
+label_size = 41;
+% scale = 3;
+stride = 41;
+
+dataset='91';
+dataset_path = 'C:\Shreyas\Programming\Python\Neural_Networks\Data\Dataset\';
+data_path = strcat(dataset_path, dataset);
+
+save_data = 'train_91_rgbchannels_matlab.h5';
+save_dataset_path_1 = 'C:\Shreyas\Programming\Python\Neural_Networks\VDSR\Data';
+save_dataset_path_2 = '\RGB2YCBCR_MATLAB\';
+save_dataset_path = strcat(save_dataset_path_1, save_dataset_path_2);
+save_path = strcat(save_dataset_path, save_data);
+if ~exist(save_dataset_path)
+    mkdir(save_dataset_path);
+end
+data = zeros(1, image_size, image_size, 1);
+label = zeros(1, label_size, label_size, 1);
+padding = abs(image_size - label_size)/2;
+count = 0;
+
+filepath = dir(fullfile(data_path, '*.bmp'));
+
+for i = 1: length(filepath)
+    img = imread(fullfile(data_path, filepath(i).name));
+    % img = rgb2ycbcr(img);
+    img_r = im2double(img(:,:,1));
+    img_g = im2double(img(:,:,2));
+    img_b = im2double(img(:,:,3));
+    [h,w] = size(img_r);
+    for scale=2:4
+        img_r_label = img_r(1:h-mod(h,scale), 1:w-mod(w,scale));
+        [h1, w1] = size(img_r_label);
+        img_r_input = imresize(imresize(img_r_label, 1/scale, 'bicubic'), [h1, w1],'bicubic');
+        img_g_label = img_g(1:h-mod(h,scale), 1:w-mod(w,scale));
+        % [h2, w2] = size(img_g_label);
+        img_g_input = imresize(imresize(img_g_label, 1/scale, 'bicubic'), [h1, w1],'bicubic');
+        img_b_label = img_b(1:h-mod(h,scale), 1:w-mod(w,scale));
+        % [h3, w3] = size(img_b_label);
+        img_b_input = imresize(imresize(img_b_label, 1/scale, 'bicubic'), [h1, w1],'bicubic');
+        % [h2, w2] = size(img_r_label);
+    
+        for x = 1:stride:h1-image_size+1
+            for y = 1:stride:w1-image_size+1
+
+                subimg_r_input = img_r_input(x:x+image_size-1, y:y+image_size-1);
+                subimg_r_label = img_r_label(x:x+label_size-1, y:y+label_size-1);
+                subimg_g_input = img_g_input(x:x+image_size-1, y:y+image_size-1);
+                subimg_g_label = img_g_label(x:x+label_size-1, y:y+label_size-1);
+                subimg_b_input = img_b_input(x:x+image_size-1, y:y+image_size-1);
+                subimg_b_label = img_b_label(x:x+label_size-1, y:y+label_size-1);
+
+%                 count = count + 1;
+%                 data(1,:,:,count) = subimg_r_input;
+%                 label(1,:,:,count) = subimg_r_label;
+%                 data(2,:,:,count) = subimg_g_input;
+%                 label(2,:,:,count) = subimg_g_label;
+%                 data(3,:,:,count) = subimg_b_input;
+%                 label(3,:,:,count) = subimg_b_label;
+%                 count = count + 1;
+%                 data(1,:,:,count) = fliplr(subimg_r_input);
+%                 label(1,:,:,count) = fliplr(subimg_r_label);
+%                 data(2,:,:,count) = fliplr(subimg_g_input);
+%                 label(2,:,:,count) = fliplr(subimg_g_label);
+%                 data(3,:,:,count) = fliplr(subimg_b_input);
+%                 label(3,:,:,count) = fliplr(subimg_b_label);
+%                 count = count + 1;
+%                 data(1,:,:,count) = flipud(subimg_r_input);
+%                 label(1,:,:,count) = flipud(subimg_r_label;
+%                 data(2,:,:,count) = flipud(subimg_g_input);
+%                 label(2,:,:,count) = flipud(subimg_g_label);
+%                 data(3,:,:,count) = flipud(subimg_b_input);
+%                 label(3,:,:,count) = flipud(subimg_b_label);
+                
+                for degree=0:90:270
+                    count = count + 1;
+                    data(1,:,:,count) = imrotate(subimg_r_input, degree);
+                    label(1,:,:,count) = imrotate(subimg_r_label, degree);
+                    data(2,:,:,count) = imrotate(subimg_g_input, degree);
+                    label(2,:,:,count) = imrotate(subimg_g_label, degree);
+                    data(3,:,:,count) = imrotate(subimg_b_input,degree);
+                    label(3,:,:,count) = imrotate(subimg_b_label, degree);
+                    count = count + 1;
+                    data(1,:,:,count) = imrotate(fliplr(subimg_r_input),degree);
+                    label(1,:,:,count) = imrotate(fliplr(subimg_r_label),degree);
+                    data(2,:,:,count) = imrotate(fliplr(subimg_g_input),degree);
+                    label(2,:,:,count) = imrotate(fliplr(subimg_g_label),degree);
+                    data(3,:,:,count) = imrotate(fliplr(subimg_b_input),degree);
+                    label(3,:,:,count) = imrotate(fliplr(subimg_b_label),degree);
+%                     count = count + 1;
+%                     data(1,:,:,count) = imrotate(flipud(subimg_r_input),degree);
+%                     label(1,:,:,count) = imrotate(flipud(subimg_r_label),degree);
+%                     data(2,:,:,count) = imrotate(flipud(subimg_g_input),degree);
+%                     label(2,:,:,count) = imrotate(flipud(subimg_g_label),degree);
+%                     data(3,:,:,count) = imrotate(flipud(subimg_b_input),degree);
+%                     label(3,:,:,count) = imrotate(flipud(subimg_b_label),degree);
+                end
+            end
+        end
+    end
+end
+order = randperm(count);
+data = data(:, :, :, order);
+label = label(:, :, :, order);
+
+h5create(save_path,'/data',[3 image_size image_size count]);
+h5create(save_path,'/label',[3 label_size label_size count]);
+h5write(save_path,'/data',data);
+h5write(save_path,'/label',label);
+h5disp(save_path);
---
+++ VDSR/data_gen_h5_matlab_ychannel_v2.m
@@ -0,0 +1,79 @@
+clc;
+clear all;
+close all;
+
+image_size = 41;
+label_size = 41;
+% scale = 3;
+stride = 41;
+
+dataset='291';
+dataset_path = 'C:\Shreyas\Programming\Python\Neural_Networks\Data\Dataset\';
+data_path = strcat(dataset_path, dataset);
+
+save_data = 'train_291_ychannels_matlab.h5';
+save_dataset_path_1 = 'C:\Shreyas\Programming\Python\Neural_Networks\VDSR\Data';
+save_dataset_path_2 = '\RGB2YCBCR_MATLAB\';
+save_dataset_path = strcat(save_dataset_path_1, save_dataset_path_2);
+save_path = strcat(save_dataset_path, save_data);
+
+data = zeros(1, image_size, image_size, 1);
+label = zeros(1, label_size, label_size, 1);
+% padding = abs(image_size - label_size)/2;
+count=0;
+
+filepath = dir(fullfile(data_path, '*.bmp'));
+
+for i = 1: length(filepath)
+    img = imread(fullfile(data_path, filepath(i).name));
+    if length(size(img))~=3
+      img = cat(3, img, img, img);
+    end
+    img = rgb2ycbcr(img);
+    img = im2double(img(:,:,1));
+    [h,w] = size(img);
+    for scale = 2:4
+        img_label = img(1:h-mod(h,scale), 1:w-mod(w,scale));
+        [h1, w1] = size(img_label);
+        img_input = imresize(imresize(img_label, 1/scale, 'bicubic'), [h1, w1],'bicubic');
+
+        for x = 1:stride:h1-image_size+1
+            for y = 1:stride:w1-image_size+1
+
+                subimg_input = img_input(x:x+image_size-1, y:y+image_size-1);
+                subimg_label = img_label(x:x+label_size-1, y:y+label_size-1);
+
+%                 count = count + 1;
+%                 data(1,:,:,count) = subimg_input;
+%                 label(1,:,:,count) = subimg_label;
+%                 count = count + 1;
+%                 data(1,:,:,count) = fliplr(subimg_input);
+%                 label(1,:,:,count) = fliplr(subimg_label);
+%                 count = count + 1;
+%                 data(1,:,:,count) = flipud(subimg_input);
+%                 label(1,:,:,count) = flipud(subimg_label);
+                
+                for degree = 0:90:270
+                    count = count + 1;
+                    data(1,:,:,count) = imrotate(subimg_input, degree);
+                    label(1,:,:,count) = imrotate(subimg_label,degree);
+                    count = count + 1;
+                    data(1,:,:,count) = fliplr(imrotate(subimg_input, degree));
+                    label(1,:,:,count) = fliplr(imrotate(subimg_label, degree));
+%                     count = count + 1;
+%                     data(1,:,:,count) = flipud(imrotate(subimg_input, degree));
+%                     label(1,:,:,count) = flipud(imrotate(subimg_label, degree));
+                end
+            end
+        end
+    end
+end
+order = randperm(count);
+data = data(1, :, :, order);
+label = label(1, :, :, order);
+
+h5create(save_path,'/data',[1 image_size image_size count]);
+h5create(save_path,'/label',[1 label_size label_size count]);
+h5write(save_path,'/data',data);
+h5write(save_path,'/label',label);
+h5disp(save_path);
